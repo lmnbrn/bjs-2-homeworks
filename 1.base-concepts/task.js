b4617d03227d307884ecf79b8a6cd0ac140a3d17
lmnbrn <limeinbrain@gmail.com>
2022-08-09 18:45:18 +0300

    add task1 solution 2nd attempt

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,25 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let discriminant, squareRoot;
+
+  discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant === 0) {
+    squareRoot = -b / (2 * a);
+    arr.push(squareRoot);
+  } else {
+    if (discriminant > 0) {
+      console.log(Math.sqrt(discriminant));
+      console.log(-b + Math.sqrt(discriminant));
+      console.log(2 * a);
+      squareRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+      arr.push(squareRoot);
+      squareRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+      arr.push(squareRoot);
+    }
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
