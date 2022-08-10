c886d186ba94fdb4f33549e32bb44c0d4aa3ddc3
lmnbrn <limeinbrain@gmail.com>
2022-08-10 13:22:43 +0300

    add tasks solution

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,27 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min, max, sum, avgStr, avg;
 
-  // Ваш код
+  min = max = arr[0];
+  sum = 0;
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i];
+      sum = sum + arr[i];
+    }
+    else if(arr[i] > max) {
+      max = arr[i];
+      sum = sum + arr[i];
+    }
+    else {
+      sum = sum + arr[i];
+    }
+  }
+    
+  avg = sum / arr.length;
+  avgStr = avg.toFixed(2);
+  avg = Number(avgStr);
 
   return { min: min, max: max, avg: avg };
 }
@@ -10,22 +29,42 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
+  sum = 0;
+  for(let i=0; i< arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
+  max = 0;
+  for(let i = 0; i < arrOfArr.length; i++) {    
+    if(func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-}
+  let min, max, interval;
+  min = max = arr[0];
+  interval = 0;
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+    else if(arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  
+  interval = Math.abs(max - min);
+  
+  return interval;
+};
