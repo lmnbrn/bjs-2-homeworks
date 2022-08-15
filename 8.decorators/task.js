402e78e91e07b84ddb6fcfc9615c7471dc7c0507
lmnbrn <limeinbrain@gmail.com>
2022-08-15 14:47:01 +0300

    add tasks solutions

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,39 @@
+//Задача 1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find(item => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func.call(this, ...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {cache.shift()}
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
-
+//Задача 2
 function debounceDecoratorNew(func) {
-  // Ваш код
-}
+  let timerID = null;
+  let repeatCall = false;
 
-function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper(...rest) {
+    clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      repeatCall = false;
+      return func.call(...rest);
+    });
+    if (!repeatCall) {
+      repeatCall = true;
+      func.call(...rest);
+    }
+  }
+  return wrapper;
 }
